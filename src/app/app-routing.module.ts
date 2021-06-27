82be4eafce77300ca529b6b7c9b7f9e2d281538d
Richard Calderan <richardcck@hotmail.com>
2021-06-27 09:24:08 -0300

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PersonComponent } from './person/person.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  { path: 'persons', component: PersonComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
